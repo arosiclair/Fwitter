b21767f5810f63e97f42e2e44ecd7a919cd3370b
arosiclair <arosiclair@gmail.com>
2017-03-23 22:40:15 -0400

    allowed cse356 machine ip as host

--- fwitter_app/settings.py
+++ fwitter_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_j(=mes-=z)4!lk*j@9!2&b=go@bx4br3y&ekp67w^*ek!=8!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'arosiclair.cse356.compas.cs.stonybrook.edu']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.125']
 
 
 # Application definition
