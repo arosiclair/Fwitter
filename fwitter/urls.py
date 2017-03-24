f2248041fe3cca9b7972840f4fb0dd19c88f18a7
arosiclair <arosiclair@gmail.com>
2017-03-23 23:55:28 -0400

    added missing additem urlconf

--- fwitter/urls.py
+++ fwitter/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import controller
 
 urlpatterns = [
+    url(r'^$', controller.index),
     url(r'^adduser$', controller.adduser),
     url(r'^verify$', controller.verify),
     url(r'^login$', controller.login),
     url(r'^logout$', controller.logout),
+    url(r'^additem$', controller.additem),
 ]
