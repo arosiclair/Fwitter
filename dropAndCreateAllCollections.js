755b975d60a8770af4ed1eb65f291e3f7baecbe2
arosiclair <arosiclair@gmail.com>
2017-05-15 16:17:06 -0400

    updated drop script to clear separate media cluster

--- dropAndCreateAllCollections.js
+++ dropAndCreateAllCollections.js
@@ -4,20 +4,24 @@ db = conn.getDB("Fwitter");
 
 db.Users.drop();
 db.Tweets.drop();
-db.Media.drop();
 db.django_session.drop();
 
 db.createCollection("Users");
 db.createCollection("Tweets");
-db.createCollection("Media")
 db.createCollection("django_session");
 
 sh.shardCollection("Fwitter.Users", { "_id" : "hashed" } );
 sh.shardCollection("Fwitter.Tweets", { "_id" : "hashed" } );
-sh.shardCollection("Fwitter.Media", { "_id" : "hashed" } );
 sh.shardCollection("Fwitter.django_session", { "_id" : "hashed" } );
 
 db.Tweets.createIndex({ "timestamp": -1 });
 db.Tweets.createIndex({ "likes": -1 });
 db.Tweets.createIndex({ "parent": 1 });
 db.Tweets.createIndex({ "content": "text"});
+
+conn = new Mongo("192.168.1.80:27017");
+db = conn.getDB("Fwitter");
+
+db.Media.drop();
+db.createCollection("Media");
+sh.shardCollection("Fwitter.Media" { "_id", "hashed" });
